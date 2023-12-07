ad016b7355ce04796f7a82ea29466cb31cbbb954
moGaara <mohamedsayed415.ms@gmail.com>
2023-12-07 13:08:58 +0200

    Adding rest of files

--- src/Fuzzifier.java
+++ src/Fuzzifier.java
@@ -1,3 +1,40 @@
-public class Fuzzifier {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class Fuzzifier
+{
+
+
+    private List<Variables>variablesList;
+
+
+
+    List<Variables> fuzzification(Variables currentVariable, Double crispValue)
+    {
+        
+
+        return variablesList;
+    }
+
+    Fuzzifier()
+    {
+        variablesList = new ArrayList<>();
+    }
+
+    private void AddElementToVariablesList(Variables variable)
+    {
+        variablesList.add(variable);
+    }
+
+
+    public List<Variables> getVariablesList()
+    {
+        return variablesList;
+    }
+
+    public void setVariablesList(List<Variables> variablesList)
+    {
+        this.variablesList = variablesList;
+    }
 }
--- src/FuzzySet.java
+++ src/FuzzySet.java
@@ -1,2 +1,56 @@
-public class FuzzySet {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuzzySet
+{
+
+
+    private String name,type;
+    private List<Integer>values;
+
+    FuzzySet(String name, String type,List<Integer> values)
+    {
+        setName(name);
+        setType(type);
+        if(type.equals("TRI")) values = new ArrayList<>(3);
+        else values = new ArrayList<>(4);
+
+        setValues(values);
+
+    }
+
+    public List<Integer> getValues()
+    {
+        return values;
+    }
+
+    public void setValues(List<Integer> values)
+    {
+        this.values = values;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+
 }
