160e6046be2dd9e1654bc332c50b3af8d6d0992e
newYasser <mohammedyasser0110@gmail.com>
2023-12-07 14:28:37 +0200

    editing the inferance logiv note:not completed

--- src/Inference.java
+++ src/Inference.java
@@ -1,2 +1,26 @@
+import java.util.List;
+import java.util.Map;
+
 public class Inference {
+    private List<Rule>rules;
+    private Variables outputVariable;
+    private Map<String,Variables>fuzzifiedVariables;
+    public Inference(Map<String,Variables> fuzzifiedVariables){
+        this.fuzzifiedVariables = fuzzifiedVariables;
+    }
+    private Variables getOutputVariable(String outputVariable){
+        return fuzzifiedVariables.get(outputVariable);
+    }
+    private void solveRules(){
+        for(Rule i:rules){
+            String[]rule= i.getRule();
+            for(int j = 0; j < rule.length;++j){
+                if(rule[j].equals("not")){
+                    // get the fuzzication answer and update it 
+                }
+            }
+        }
+    }
+
+
 }
